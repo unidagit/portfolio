dfc2eda6e4cd918450d44274b8f63afc743d8ee6
yunheejo <rhcjrdbsgml@gmail.com>
2022-08-29 20:19:19 +0900

    모달 기능 추가

---
+++ assets/js/modal.js
@@ -0,0 +1,21 @@
+const modalViews = document.querySelectorAll(".project-modal");
+const modalBtns = document.querySelectorAll(".modal-button");
+const modalCloses = document.querySelectorAll(".project-modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
